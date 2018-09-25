0841a1c9cd23c2b14492ad30ccabdca42050e4b7
fengcheng13lu <lixin_show@163.com>
2018-09-25 10:27:38 +0800

    letao

---
+++ public/m/abc.vue
@@ -0,0 +1,31 @@
+<<<<<<< HEAD
+  <header class="mui-bar mui-bar-nav my-header">
+        <a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left"></a>
+        <h1 class="mui-title" v-for="" @click="">注册页面</h1>
+        
+        
+    </header>
+ <style>
+    
+</style>
+
+  <script>
+
+    
+    </script>
+=======
+  <header class="mui-bar mui-bar-nav my-header">
+        <a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left"></a>
+        <h1 class="mui-title" v-for="" @click="">注册页面</h1>
+        
+        
+    </header>
+ <style>
+    
+</style>
+
+  <script>
+
+    
+    </script>
+>>>>>>> f014a6b6b72bf8033d698a322e49209185fe5de3
